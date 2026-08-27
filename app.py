6e69449d0f5d1eac4c1954bc5a65ca4ac658b33b
ballababu21-ai <ballababu21@gmail.com>
2026-08-27 22:05:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import requests
-import plotly.graph_objects as go
 from datetime import datetime
 
 # -------------------------------------------------------------------
